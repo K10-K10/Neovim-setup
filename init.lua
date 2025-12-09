572009e7dd8cc85564e3958ed4305996ea8f2291
K10-K10 <keito.tottori@gmail.com>
2025-12-09 21:38:04 +0900

    update for lazyVim

--- init.lua
+++ init.lua
@@ -1,9 +1,8 @@
--- Basic Options
-require('options')
-
--- Keymaps
-require('keymaps')
-
--- Plugin
-require('plugins')
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.c", "*.cpp", "*.h", "*.hpp" },
+  callback = function()
+    vim.cmd("silent! !clang-format -i %")
+  end,
+})
---
+++ lua/config/formatteing.lua
@@ -0,0 +1,6 @@
+formatters_by_ft = {
+  c = { "clang_format" },
+  cpp = { "clang_format" },
+  hpp = {"clang_format"},
+  h = {"clang_format"},
+},
